5e3c429277f83ee6533b706e865d2ee4e9d1db82
nnAsaddd <nnAsaddd@gmail.com>
2024-08-10 12:11:22 +0500

    Initial Commit
    template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShoeMart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
